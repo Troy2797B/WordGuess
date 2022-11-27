bbbe23f02ad4d6ed2ff9fed4c4585b0002422f15
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-26 21:29:36 -0500

    DOOONE

--- src/main/java/com/github/zipcodewilmington/sample/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/sample/Wordguess.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington.sample;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -20,67 +22,93 @@ public class Wordguess {
 
     public boolean checkIfPlayerGuessIsALetter(String input) {
         //while it isn't the end and user didn't choose a letter yet
-            //if the input is only one character long AND is a letter
-            if (input.matches("[a-zA-Z]") && input.length() == 1) {
-                //turn it into a char
+        //if the input is only one character long AND is a letter
+        if (input.matches("[a-zA-Z]") && input.length() == 1) {
+            //turn it into a char
 //                input.charAt(0);
-                return true;
-            } else if (input.length() > 1) {
-                System.out.println("Please enter only a letter");
-                return false;
-            } else if (!input.matches("[a-zA-Z]")) {
-                System.out.println("Please enter a character in the alphabet.");
-                return false;
-            }
+            return true;
+        } else if (input.length() > 1) {
+            System.out.println("Please enter only a letter");
             return false;
+        } else if (!input.matches("[a-zA-Z]")) {
+            System.out.println("Please enter a character in the alphabet.");
+            return false;
+        }
+        return false;
     }
 
-    public boolean checkIfGuessedLetterMatches(char playerLetter, char[] charArray){
+    public boolean checkIfGuessedLetterMatches(char playerLetter, char[] charArray) {
         //loop through the given character array
-        for (int i = 0; i < 3; i++){
-            if (charArray[i] == playerLetter){
+        for (int i = 0; i < 3; i++) {
+            if (charArray[i] == playerLetter) {
                 return true;
             }
-        } return false;
+        }
+        return false;
         //if the index matches, return true
     }
 
-    public void replaceArrayElementWithCorrectLetter(char playerLetter, char [] randomWord){
-        for(int i = 0; i < 3; i++){
-            if (randomWord[i] == playerLetter){
+    public void replaceArrayElementWithCorrectLetter(char playerLetter, char[] randomWord) {
+        for (int i = 0; i < 3; i++) {
+            if (randomWord[i] == playerLetter) {
                 playerGuess[i] = playerLetter;
             }
         }
     }
 
-    public void run(){
+    public void run() {
         //set the word
-        char[] word = randomWordToCharArray();
-        System.out.println(word);
-        boolean end = false;
-        int numberOfGuesses = 0;
-        do {
 
-            System.out.println("Guess a letter");
-            String guess = userInput.nextLine();
-             if (!checkIfPlayerGuessIsALetter(guess)){
-                 //loops back to guess a letter
-            } else if(checkIfPlayerGuessIsALetter(guess)){
-                 //turn into a character
-                 char playerLetter = guess.charAt(0);
-                 System.out.println("You chose : "+ playerLetter + ".");
-                 //then continue the game
-                 //checking if the letter matches anything or not
-                 if (!checkIfGuessedLetterMatches(playerLetter, word)){
-                     numberOfGuesses++;
-                     System.out.println("Too bad! You used "+ numberOfGuesses + " guess.");
-                     break;
-                 } else if (checkIfGuessedLetterMatches(playerLetter, word)){
-                     replaceArrayElementWithCorrectLetter(playerLetter, word);
-                     System.out.println(playerGuess);
-                 }
-             }
-        } while (numberOfGuesses < 5 || !end);
+        String playAgain = "";
+        boolean restart = true;
+        do {//making a new word, starting the number of guesses over
+            char[] word = randomWordToCharArray();
+            int numberOfGuesses = 0;
+            boolean end = false;
+            do {
+                System.out.println("Guess a letter");
+                String guess = userInput.nextLine();
+                if (!checkIfPlayerGuessIsALetter(guess)) {
+                    //loops back to guess a letter
+                } else if (checkIfPlayerGuessIsALetter(guess)) {
+                    //turn into a character
+                    char playerLetter = guess.charAt(0);
+                    System.out.println("You chose : " + playerLetter + ".");
+                    //then continue the game
+                    //checking if the letter matches anything or not
+                    if (!checkIfGuessedLetterMatches(playerLetter, word)) {
+                        numberOfGuesses++;
+                        System.out.println("Too bad! You used " + numberOfGuesses + " guess.");
+                        //checking if number of guesses is equal to three and breaking out if true
+                        if (numberOfGuesses == 3) {
+                            System.out.println("You lost.");
+                            Arrays.fill(playerGuess, '_');
+                            end = true;
+                        }
+                    } else if (checkIfGuessedLetterMatches(playerLetter, word)) {
+                        replaceArrayElementWithCorrectLetter(playerLetter, word);
+                        System.out.println(playerGuess);
+                        if (Arrays.toString(playerGuess).equals(Arrays.toString(word))){
+                            System.out.println("CONGRAAAATS YOU WOOOOOOON!!!");
+                            Arrays.fill(playerGuess, '_');
+                            end = true;
+                        }
+                    }
+                }
+
+
+
+            } while (!end);
+            System.out.println("Would you like to play again? Y/N?");
+            //if the user does, loop back up to the top, if not, restart = true
+            String answer = userInput.nextLine();
+            if (answer.equals("n")) {
+                System.out.println("Thank you for playing! Have a great day. :)");
+                restart = false;
+            } else if (answer.equals("y")) {
+               restart = true;
+            }
+        } while (restart);
+
     }
-
 }
